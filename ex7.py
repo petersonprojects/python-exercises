1dc7237192c6b73f3e465f2106abcc752ee2f31b
Micah Peterson <micahpeterson@live.com>
2020-06-02 16:46:47 -0500

    ex7

---
+++ ex7.py
@@ -0,0 +1,22 @@
+bill = float(input("Bill amount >>> "))
+service = input("Level of service (good, fair or bad)>>> ")
+
+if service == "good":
+    tipAmt = bill*0.2
+    print(f'Tip Amount: ${tipAmt}')
+    total = bill + tipAmt
+    print(f'Total amount: ${total})')
+
+if service == "fair":
+    tipAmt = bill*0.15
+    print(f'Tip Amount: ${tipAmt}')
+    total = bill + tipAmt
+    rounded = round(total,2)
+    print(f'Total amount: ${rounded}')
+    
+if service == "bad":
+    tipAmt = bill*0.1
+    print(f'Tip Amount: ${tipAmt}')
+    total = bill + tipAmt
+    rounded = round(total,2)
+    print(f'Total amount: ${rounded}')
