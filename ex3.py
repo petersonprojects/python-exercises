0e71e323743e98997400b364f5628ab7fc4e03bc
Micah Peterson <micahpeterson@live.com>
2020-06-02 15:51:34 -0500

    ex3

---
+++ ex3.py
@@ -0,0 +1,7 @@
+print("Please fill in the blank below: ")
+print("___name___'s favorite subject in school is ___(subject)___")
+
+name = input("What is name? >>> ")
+subject = input("What is subject? >>> ")
+
+print(f"{name}'s favorite subject in school is {subject}.")
